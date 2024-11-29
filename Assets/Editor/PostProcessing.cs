e9512c207fca9be0f6f1b7b46de0ebedcac08b85
DikobrotherIT <dikobrotherlive@gmail.com>
2024-11-29 11:20:37 +0200

    PostProcessing

--- Assets/Editor/PostProcessing.cs
+++ Assets/Editor/PostProcessing.cs
@@ -21,7 +21,16 @@ public class IosBuildPostprocessor
 
         // Add ITSAppUsesNonExemptEncryption to Info.plist
         rootDict.SetString("ITSAppUsesNonExemptEncryption", "false");
-        
+        string projectPath = path + "/Unity-iPhone.xcodeproj/project.pbxproj";
+        PBXProject pbxProject = new PBXProject();
+        pbxProject.ReadFromFile(projectPath);
+
+        // Getting the UnityFramework Target and changing build settings
+        string tar = pbxProject.GetUnityFrameworkTargetGuid();
+        pbxProject.SetBuildProperty(tar, "ALWAYS_EMBED_SWIFT_STANDARD_LIBRARIES", "NO");
+
+        // After we're done editing the build settings we save it 
+        pbxProject.WriteToFile(projectPath);
 
 
         File.WriteAllText(plistPath, plist.WriteToString());
